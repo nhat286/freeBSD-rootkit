fde9c1ed2fb312551ed893218cc97164a9394b03
User Comp6447 <comp6447@rootkit-dev.6447>
2018-10-14 07:38:29 +1100

    started code on port checking

---
+++ rootkits/detection/port_check/port_check.c
@@ -0,0 +1,133 @@
+/*-
+ * Copyright (c) 2007 Joseph Kong.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any co-contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/proc.h>
+#include <sys/module.h>
+#include <sys/sysent.h>
+#include <sys/kernel.h>
+#include <sys/systm.h>
+#include <sys/queue.h>
+#include <sys/socket.h>
+#include <sys/sysproto.h>
+
+#include <net/if.h>
+#include <netinet/in.h>
+#include <netinet/in_pcb.h>
+#include <netinet/ip_var.h>
+#include <netinet/tcp_var.h>
+
+/*
+Objectives:
+-> check ipi_listhead and get "count"
+-> get ipi_count and compare
+-> get porthasebase and compare
+-> get hashbase and compare 
+
+Possible counter: UDP ports .... 
+*/
+
+/* System call to hide an open port. */
+static int
+port_checker(struct thread *td, void *syscall_args)
+{
+	//struct port_hiding_args *uap;
+	//uap = (struct port_hiding_args *)syscall_args;
+
+	struct inpcb *inpb;
+    int untrusted_count = 0;
+    // we iterate through ipi_listhead and get the supposed number of open ports
+	INP_INFO_WLOCK(&tcbinfo);
+
+	LIST_FOREACH(inpb, tcbinfo.ipi_listhead, inp_list) {
+        untrusted_count++;
+    }
+	/* Iterate through the TCP-based inpcb list. */
+    /*
+	LIST_FOREACH(inpb, tcbinfo.ipi_listhead, inp_list) {
+		if (inpb->inp_vflag & INP_TIMEWAIT)
+			continue;
+
+		INP_WLOCK(inpb);
+
+		// Do we want to hide this local open port?
+		if (uap->lport == ntohs(inpb->inp_inc.inc_ie.ie_lport))
+			LIST_REMOVE(inpb, inp_list);
+
+        // newer version now specifies read/write locks, we just take write
+		INP_WUNLOCK(inpb);
+	}
+    */
+	INP_INFO_WUNLOCK(&tcbinfo);
+
+    //print out this count
+    printf("Untrusted inp_list count: %d\n", untrusted_count);
+
+    
+    // Now get ipi_count
+    printf("ipi_count var says: %d\n", tcbinfo.ipi_count);
+
+	return(0);
+}
+
+/* The sysent for the new system call. */
+static struct sysent port_checker_sysent = {
+	0,			    /* number of arguments */
+                    // our syscall doesn't take in any args 
+	port_checker		/* implementing function */
+};
+
+/* The offset in sysent[] where the system call is to be allocated. */
+static int offset = NO_SYSCALL;
+
+/* The function called at load/unload. */
+static int
+load(struct module *module, int cmd, void *arg)
+{
+	int error = 0;
+
+	switch (cmd) {
+	case MOD_LOAD:
+		uprintf("Port count checker loaded at offset [%d].\n", offset);
+		break;
+
+	case MOD_UNLOAD:
+		uprintf("Port count checker unloaded from offset [%d].\n", offset);
+		break;
+
+	default:
+		error = EOPNOTSUPP;
+		break;
+	}
+
+	return(error);
+}
+
+SYSCALL_MODULE(port_checker, &offset, &port_checker_sysent, load, NULL);
